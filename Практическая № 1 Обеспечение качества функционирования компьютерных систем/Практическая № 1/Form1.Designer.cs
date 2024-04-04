fa17f0021780e898f3c207501aff5315e7fb74b6
GLASSIK <pignat341@gmail.com>
2024-04-04 10:23:39 +0700

    hfhfbh

---
+++ Практическая № 1 Обеспечение качества функционирования компьютерных систем/Практическая № 1/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Практическая___1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.X = new System.Windows.Forms.TextBox();
+            this.H = new System.Windows.Forms.TextBox();
+            this.Y = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(666, 453);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // X
+            // 
+            this.X.Location = new System.Drawing.Point(671, 116);
+            this.X.Name = "X";
+            this.X.Size = new System.Drawing.Size(100, 22);
+            this.X.TabIndex = 1;
+            // 
+            // H
+            // 
+            this.H.Location = new System.Drawing.Point(671, 246);
+            this.H.Name = "H";
+            this.H.Size = new System.Drawing.Size(100, 22);
+            this.H.TabIndex = 2;
+            // 
+            // Y
+            // 
+            this.Y.Location = new System.Drawing.Point(671, 188);
+            this.Y.Name = "Y";
+            this.Y.Size = new System.Drawing.Size(100, 22);
+            this.Y.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(680, 141);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Длина X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(690, 213);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ширина Y";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(690, 271);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Высота H";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(685, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Рисовать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Y);
+            this.Controls.Add(this.H);
+            this.Controls.Add(this.X);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox X;
+        private System.Windows.Forms.TextBox H;
+        private System.Windows.Forms.TextBox Y;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Практическая № 1 Обеспечение качества функционирования компьютерных систем/Практическая № 1/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Практическая___1
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Bitmap bmp;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x = Convert.ToInt32(X.Text);
+            int y = Convert.ToInt32(H.Text);
+            int h = Convert.ToInt32(Y.Text);
+            Pen p = new Pen(Color.Red);
+            p.Width = 5;
+            g.DrawRectangle(p, x, y, h, h);
+            Point p1 = new Point(x, y);
+            Point p2 = new Point(x + h, y);
+            Point p3 = new Point(x + h, y + h);
+            Point p4 = new Point(x, y + h);
+            g.DrawLine(p, p1, p3);
+            g.DrawLine(p, p2, p4);
+            SolidBrush SB = new SolidBrush(Color.Green);
+            int h2 = h / 4;
+            int k = h / 8;
+            g.FillEllipse(SB, x + k + k + k, y + k, h2, h2);
+            g.FillEllipse(SB, x + k + h / 2 - k - k, y + k + h / 2, h2, h2);
+            pictureBox1.Refresh();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bmp);
+            pictureBox1.Image = bmp;
+            pictureBox1.BackColor = Color.LightSkyBlue;
+
+        }
+    }
+}
