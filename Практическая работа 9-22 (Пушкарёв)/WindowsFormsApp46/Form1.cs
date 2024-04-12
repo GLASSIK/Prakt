58ca3ba3c10881290747005ca2385bb3f2271654
GLASSIK <pignat341@gmail.com>
2024-04-12 21:04:11 +0700

    gdfggdf

---
+++ Практическая работа 9-22 (Пушкарёв)/WindowsFormsApp46/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+
+namespace WindowsFormsApp46
+{
+    public partial class Form1 : Form
+    {
+        struct produkt
+        {
+           public string naim;
+           public string stran;
+           public int koli;
+            public int stoim;
+        }
+        List<produkt> produkt1 = new List<produkt>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*
+            if (textBox1.Text.Trim().Length == 0) 
+            {
+                MessageBox.Show("Вы не ввели наименование");
+                textBox1.Focus();
+            }
+            else if (textBox2.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Вы не ввели наименование страны импортёра");
+                textBox2.Focus();
+            }
+            else if (textBox3.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Вы не ввели обьём партии");
+                textBox3.Focus();
+            }
+            else if (textBox4.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Вы не ввели стоймость за еденицу товара");
+                textBox4.Focus();
+            }
+            else if (Convert.ToInt32(textBox3.Text)<0)
+            {
+                MessageBox.Show("годовой импорт не может быть меньше 0");
+                textBox3.Focus();
+            }
+            else if (Convert.ToInt32(textBox4.Text) < 0)
+            {
+                MessageBox.Show("стоимость за еденицу товара не может быть меньше 0");
+                textBox4.Focus();
+            }
+            else
+            {
+                produkt p = new produkt();
+                p.naim = textBox1.Text;
+                p.stran = textBox2.Text;
+                p.koli = Convert.ToInt32(textBox3.Text);
+                p.stoim = Convert.ToInt32(textBox4.Text);
+                produkt1.Add(p);
+               
+               
+                matr.RowCount = produkt1.Count;
+                for (int i = 0; i < produkt1.Count; i++)
+                {
+                    matr[0, i].Value = produkt1[i].naim;
+                    matr[1, i].Value = produkt1[i].stran;
+                    matr[2, i].Value = produkt1[i].koli.ToString();
+                    matr[3, i].Value = produkt1[i].stoim.ToString();
+                }
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+                textBox4.Text = "";
+            }
+            */
+            Form2 f = new Form2();
+            if (f.ShowDialog() == DialogResult.OK)
+            {
+                produkt p = new produkt();
+                p.naim = f.textBox1.Text;
+                p.stran = f.textBox2.Text;
+                p.koli = Convert.ToInt32(f.textBox3.Text);
+                p.stoim = Convert.ToInt32(f.textBox4.Text);
+                produkt1.Add(p);
+
+
+                matr.RowCount = produkt1.Count;
+                for (int i = 0; i < produkt1.Count; i++)
+                {
+                    matr[0, i].Value = produkt1[i].naim;
+                    matr[1, i].Value = produkt1[i].stran;
+                    matr[2, i].Value = produkt1[i].koli.ToString();
+                    matr[3, i].Value = produkt1[i].stoim.ToString();
+                }
+
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (matr.CurrentRow == null)
+            {
+                MessageBox.Show("Вы не выделили строку");
+            }
+            else
+            {
+                DialogResult result = MessageBox.Show("Точно удалить элемент?", "Точно удалить элемент?", MessageBoxButtons.OKCancel,MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+                if (result == DialogResult.OK)
+                {
+                    int n = matr.CurrentRow.Index;
+                    produkt1.RemoveAt(n);
+                    matr.RowCount = produkt1.Count;
+                    for (int i = 0; i < produkt1.Count; i++)
+                    {
+                        matr[0, i].Value = produkt1[i].naim;
+                        matr[1, i].Value = produkt1[i].stran;
+                        matr[2, i].Value = produkt1[i].koli.ToString();
+                        matr[3, i].Value = produkt1[i].stoim.ToString();
+                    }
+                }
+
+            }
+        }
+
+        private void matr_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+
+        private void matr_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            StreamWriter wr = new StreamWriter("1.txt") ;
+            for (int i = 0;i < produkt1.Count; i++)
+            {
+                wr.WriteLine(matr[0, i].Value.ToString());
+                wr.WriteLine(matr[1, i].Value.ToString());
+                wr.WriteLine(matr[2, i].Value.ToString());
+                wr.WriteLine(matr[3, i].Value.ToString());
+            }
+            wr.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            produkt1.Clear();
+            StreamReader read = new StreamReader("1.txt");
+            while(read.EndOfStream== false)
+            {
+                produkt p = new produkt();
+                p.naim = read.ReadLine();
+                p.stran = read.ReadLine();
+                p.koli = Convert.ToInt32(read.ReadLine());
+                p.stoim = Convert.ToInt32(read.ReadLine());
+                produkt1.Add(p);
+            }
+            matr.RowCount = produkt1.Count;
+            for (int i = 0; i < produkt1.Count; i++)
+            {
+                matr[0, i].Value = produkt1[i].naim;
+                matr[1, i].Value = produkt1[i].stran;
+                matr[2, i].Value = produkt1[i].koli.ToString();
+                matr[3, i].Value = produkt1[i].stoim.ToString();
+            }
+            read.Close();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < produkt1.Count; i++)
+            {
+                for (int j = 0; j < produkt1.Count - i - 1; j++)
+                {
+                    if (produkt1[j].koli < produkt1[j + 1].koli)
+                    {
+                        produkt p = produkt1[j];
+                        produkt1[j] = produkt1[j + 1];
+                        produkt1[j + 1] = p;
+
+                    }
+                }
+            }
+            dataGridView1.RowCount = produkt1.Count;
+            for (int i = 0;i < produkt1.Count; i++)
+            {
+                dataGridView1[0,i].Value = produkt1[i].naim;
+                dataGridView1[1, i].Value = produkt1[i].stran;
+                dataGridView1[2, i].Value = produkt1[i].koli.ToString();
+                dataGridView1[3, i].Value = produkt1[i].stoim.ToString();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form2 f = new Form2();
+            int n = matr.CurrentRow.Index;
+            f.textBox1.Text = matr[0, n].Value.ToString();
+            f.textBox2.Text = matr[1, n].Value.ToString();
+            f.textBox3.Text = matr[2, n].Value.ToString();
+            f.textBox4.Text = matr[3, n].Value.ToString();
+
+            if (f.ShowDialog() == DialogResult.OK)
+            {
+                int d = matr.CurrentRow.Index;
+                produkt p = new produkt();
+                p.naim = f.textBox1.Text;
+                p.stran = f.textBox2.Text;
+                p.koli = Convert.ToInt32(f.textBox3.Text);
+                p.stoim = Convert.ToInt32(f.textBox4.Text);
+                produkt1[d] = p;
+                matr.RowCount = produkt1.Count;
+                for (int i = 0; i < produkt1.Count; i++)
+                {
+                    matr[0, i].Value = produkt1[i].naim;
+                    matr[1, i].Value = produkt1[i].stran;
+                    matr[2, i].Value = produkt1[i].koli.ToString();
+                    matr[3, i].Value = produkt1[i].stoim.ToString();
+                }
+            }
+        }
+    }
+}
---
+++ Практическая работа 9-22 (Пушкарёв)/WindowsFormsApp46/Form2.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace WindowsFormsApp46
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(202, 20);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Обьём годового импорта";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(268, 20);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Наименование страны импортёра";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 20);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Наименование товара";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(286, 125);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(345, 26);
+            this.textBox3.TabIndex = 12;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(286, 75);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(345, 26);
+            this.textBox2.TabIndex = 11;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(286, 17);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(345, 26);
+            this.textBox1.TabIndex = 10;
+            // 
+            // button2
+            // 
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button2.Location = new System.Drawing.Point(506, 235);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 36);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Продолжить";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button1.Location = new System.Drawing.Point(55, 235);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 36);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Отмена";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 176);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(239, 20);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Стоимость товара за еденицу";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(286, 170);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(345, 26);
+            this.textBox4.TabIndex = 17;
+            // 
+            // Form2
+            // 
+            this.AcceptButton = this.button2;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.button1;
+            this.ClientSize = new System.Drawing.Size(676, 295);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form2_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.TextBox textBox3;
+        public System.Windows.Forms.TextBox textBox2;
+        public System.Windows.Forms.TextBox textBox1;
+        public System.Windows.Forms.Button button2;
+        public System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        public System.Windows.Forms.TextBox textBox4;
+    }
+}
